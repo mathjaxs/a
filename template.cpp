3dcd3b9a3f814129af3878f424d036060ac40ff0
mathjaxs <140137836+mathjaxs@users.noreply.github.com>
2023-12-23 14:15:08 +0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+void input(){
+    ios_base::sync_with_studio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve(){
+    
+}
+
+int main(){
+    freopen("input.txt", "r", stdin);
+}
